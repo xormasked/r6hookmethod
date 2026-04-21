1e36dda3902b236190102f698aba05195175daed
xormasked <rossford186@gmail.com>
2026-04-20 23:50:54 -0500

    Create hook_manager.cpp

---
+++ hook_manager.cpp
@@ -0,0 +1,26 @@
+namespace utils {
+    namespace simple_hook_manager {
+
+        template <typename Fn>
+        inline bool create( void* target, Fn detour, Fn* original )
+        {
+            if ( !target || !detour || !original )
+                return false;
+
+            if ( MH_CreateHook( target, reinterpret_cast< LPVOID >( detour ), reinterpret_cast< LPVOID* >( original ) ) != MH_OK )
+                return false;
+
+            return MH_EnableHook( target ) == MH_OK;
+        }
+
+        inline void remove( void* target )
+        {
+            if ( !target )
+                return;
+
+            MH_DisableHook( target );
+            MH_RemoveHook( target );
+        }
+
+    }
+}
